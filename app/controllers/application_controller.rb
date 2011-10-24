fef68bcc0f03db4f6038a23d128314167c02c8b6
L. Preston Sego III <pregosego111_prodigy@yahoo.com>
2011-10-24 02:11:30 -0400

    Always redirect to the root URL.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def stored_location_for(resource)
+    nil
+  end
   
   def after_sign_in_path_for(resource)
-       root_url
+      stored_location_for(resource) || root_url
   end
 end
